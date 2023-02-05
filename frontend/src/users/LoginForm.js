f6e817c618ca6526741f280205adcb0976d9bd81
Louis Mantell <loiuismantell@Louis-iMac.local>
2023-02-04 22:23:22 -0500

    jwt

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -30,10 +30,12 @@ function LoginForm() {
     
         if (response.status === 200) {
             setCurrentUser(data.user)
+            localStorage.setItem('token', data.token)
             history.push(`/`)
         } else {
             setErrorMessage(data.message)
-        }
+        }    
+    
     }
       
     
