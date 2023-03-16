a07af316cbe6780f9f6ed8621498e9741d9f81e4
Louis Mantell <loiuismantell@Louis-iMac.local>
2023-03-15 20:22:49 -0400

    rest-rant

--- backend/config/config.js
+++ backend/config/config.js
@@ -8,6 +8,15 @@ module.exports = {
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
+  "production": {
+    "username": process.env.RDS_USERNAME,
+    "password": process.env.RDS_PASSWORD,
+    "database": process.env.RDS_DB_NAME,
+    "host": process.env.RDS_HOSTNAME,
+    "port": process.env.RDS_PORT,
+    "dialect": "postgres"
+  },
+
   "test": {
     "username": "root",
     "password": null,
